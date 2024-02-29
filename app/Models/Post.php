ab4237d9e48e2ffa2028dc19f6eec5c653412a75
basakest <ab1652759879@gmail.com>
2024-02-29 21:15:06 +0800

    add $fillable property to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // 如果将 guarded 设置为空, 将允许对任意字段进行批量赋值
+    // protected $guarded = [];
+
+    /**
+     * 允许进行批量赋值的字段
+     * updated_at 和 created_at 似乎不受该属性影响?
+     *
+     * @var string[]
+     */
+    protected $fillable = ['title', 'excerpt', 'body'];
 }
