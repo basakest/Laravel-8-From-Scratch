fe11a8ff7a47cfe0f1c1910eee58faa2123d10a9
basakest <ab1652759879@gmail.com>
2024-02-29 15:50:42 +0800

    use layout in blade

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" type="text/css" href="/app.css">
+
+<body>
+    {{ $slot }}
+    {{--    {{ $content }}--}}
+</body>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-
-<title>My Blog</title>
-<link rel="stylesheet" type="text/css" href="/app.css">
-
-<body>
-<article>
-    <h1>{{ $post->title }}</h1>
-    <div>
-        {!! $post->body !!}
-    </div>
-</article>
-
-<a href="/">Go Back</a>
-</body>
+{{-- layout 是 components 目录下对应文件的前缀 --}}
+<x-layout>
+    {{-- 如果 components/layout.blade.php 中包括了 {{ $content }}, 那么这里就需要使用 <x-slot> 标签, 并在其中指定 name 属性的值, 具体如下面所示 --}}
+    {{-- 但如果文件中包括的是 {{ $slot }}, 那么相关的内容仅放在 <x-layout> 标签下即可 --}}
+    {{-- <x-slot name="content"> --}}
+        <article>
+            <h1>{{ $post->title }}</h1>
+            <div>
+                {!! $post->body !!}
+            </div>
+        </article>
+        <a href="/">Go Back</a>
+    {{-- </x-slot> --}}
+</x-layout>
