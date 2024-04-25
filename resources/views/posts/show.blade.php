c74efbfd23002de16b6d7287064bf9307b2d025c
basakest <ab1652759879@gmail.com>
2024-04-25 09:36:11 +0800

    code refactor(comment)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -60,29 +60,7 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    @auth
-                        <x-panel>
-                            <form method="POST" action="/post/{{ $post->slug }}/comments">
-                                @csrf
-                                <header class="flex items-center">
-                                    <img src="https://i.pravatar.cc/100?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full" />
-                                    <h2 class="ml-4">Want to participate?</h2>
-                                </header>
-
-                                <div>
-                                    <textarea name="body" class="w-full text-sm focus:ring" rows="5" placeholder="Quick, thing of something to say!"></textarea>
-                                </div>
-
-                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                                    <button type="submit" class="bg-blue-500 text-white uppercase font-seibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                                </div>
-                            </form>
-                    </x-panel>
-                    @else
-                        <p class="font-semibold">
-                            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">log in</a> to leave a comment.
-                        </p>
-                    @endauth
+                    @include('posts._add_comment')
 
                     @foreach($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
